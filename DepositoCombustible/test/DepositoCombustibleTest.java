46d02c575f6b1d739004a76910170a4d2cea01ff
Juan Chaves <juchacor05@gmail.com>
2018-02-26 21:24:07 +0100

    test

---
+++ DepositoCombustible/test/DepositoCombustibleTest.java
@@ -0,0 +1,37 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DepositoCombustibleTest {
+
+	@Test
+	void testGetDepositoNivel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDepositoMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstaVacio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstaLleno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFill() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConsumir() {
+		fail("Not yet implemented");
+	}
+
+}
